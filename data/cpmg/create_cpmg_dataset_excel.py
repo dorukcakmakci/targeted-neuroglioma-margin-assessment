b4e1d170c3f29d159461182c73331a7c4a8e459c
Doruk Cakmakci <doruk.cakmakci@outlook.com>
2020-11-11 00:10:18 +0300

    Add all source code regarding the analyses in the paper
    This commit contains source code for all targeted analyses for pathological classification tasks, metabolite quantification and dataset visualization for ERETIC-CPMG and CPMG sample sets.

---
+++ data/cpmg/create_cpmg_dataset_excel.py
@@ -0,0 +1,36 @@
+import random 
+import os
+import pdb 
+import pickle
+import numpy as np 
+import pandas as pd 
+
+project_base_path = "/home/doruk/glioma_quantification/"
+import sys 
+sys.path.insert(1,os.path.join(project_base_path, "data/"))
+from preprocessing_utils import preprocess_spectrum
+dataset_excel_path = os.path.join(project_base_path, "data/all_cpmg_hrmas_dataset.xlsx")
+target_dataset_excel_path = os.path.join(project_base_path, "data/cpmg/cpmg_final_dataset.xlsx")
+with open("./valid_cpmg_sample_filenames", "rb") as f:
+    target_sample_filenames = pickle.load(f)
+
+# read excel sheets to pandas dataframe
+statistics_df = pd.read_excel(dataset_excel_path, sheet_name="Tissue Sample Collection")
+quantification_df = pd.read_excel(dataset_excel_path, sheet_name="Metabolite Quantifications")
+statistics_df["Pathologic Classification"] = statistics_df["Pathologic Classification"].replace("Control ", "Control")
+
+# find the used samples from the dataset according to spectrum filenames
+index = statistics_df.index
+condition = statistics_df["Spectrum Filename"].isin(target_sample_filenames)
+dataset_indices = index[condition].tolist()
+statistics = statistics_df.iloc[dataset_indices, :].reset_index(drop=True)
+quant = quantification_df.iloc[dataset_indices, :].reset_index(drop=True)
+
+# drop relevant columns and save dataset
+statistics_dataset = statistics.drop(labels=["Availability", "Recidive", "Tumor Location", "Cellularity", "Necrosis", "Infiltration (Left)", "Infiltration (Right)", "Date of Death", "Date of Last Control", "Overall Survival (days)", "Metabolite Quantification Filename"], axis="columns")
+
+# write datasets
+writer = pd.ExcelWriter(target_dataset_excel_path)
+statistics_dataset.to_excel(writer, sheet_name="ERETIC-CPMG Dataset Statistics", na_rep="*", header=True, index=True)
+quant.to_excel(writer, sheet_name="ERETIC-CPMG Dataset Metabolites", na_rep="*", header=True, index=True)
+writer.save()
---
+++ data/cpmg/extract_fully_quantified_data.py
@@ -0,0 +1,33 @@
+import os 
+import pickle
+import numpy as np 
+import pandas as pd
+import pdb
+
+# define source and target folders 
+project_base_path = "/home/doruk/glioma_quantification/"
+source_data_path = os.path.join(project_base_path, "data/cpmg/raw/")
+target_data_path = os.path.join(project_base_path, "data/cpmg/fully_quantified/")
+
+# load source data 
+with open(os.path.join(source_data_path, "all_samples_spectra"), "rb") as f:
+    all_spectra = pickle.load(f)
+with open(os.path.join(source_data_path, "all_samples_quantification"), "rb") as f:
+    all_quantification = pickle.load(f)
+with open(os.path.join(source_data_path, "all_samples_quantification_availability"), "rb") as f:
+    all_quantification_availability = pickle.load(f)
+all_statistics = pd.read_pickle(os.path.join(source_data_path, "all_samples_statistics"))
+
+
+# generate target data 
+fully_quantified_idx = np.where(np.mean(all_quantification_availability, axis=1) == 1)[0]
+fully_quantified_samples_spectra = all_spectra[fully_quantified_idx,:]
+fully_quantified_samples_quantification = all_quantification[fully_quantified_idx,:]
+fully_quantified_samples_statistics = all_statistics.iloc[fully_quantified_idx].reset_index(drop=True)
+
+# save target data 
+with open(f"{target_data_path}fully_quantified_samples_spectra", "wb") as f:
+    pickle.dump(fully_quantified_samples_spectra, f)
+with open(f"{target_data_path}fully_quantified_samples_quantification", "wb") as f:
+    pickle.dump(fully_quantified_samples_quantification, f)
+fully_quantified_samples_statistics.to_pickle(f"{target_data_path}fully_quantified_samples_statistics")
